382324e6ae45a09f15f005f3587450aea6a6d039
root <root@ioBrocker.fritz.box>
2021-02-06 20:47:38 +0100

    initial commit Template

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'easee adapter settings': {
+        'en': 'Adapter settings for easee',
+        'de': 'Adaptereinstellungen für easee',
+        'ru': 'Настройки адаптера для easee',
+        'pt': 'Configurações do adaptador para easee',
+        'nl': 'Adapterinstellingen voor easee',
+        'fr': "Paramètres d'adaptateur pour easee",
+        'it': "Impostazioni dell'adattatore per easee",
+        'es': 'Ajustes del adaptador para easee',
+        'pl': 'Ustawienia adaptera dla easee',
+        'zh-cn': 'easee的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
