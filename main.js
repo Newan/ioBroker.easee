72e68d542b973eadeb70a79527c28e1ddd63d3a8
TA2k <tombox2020@gmail.com>
2022-03-01 22:58:50 +0100

    Move Signal values to debug

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ class Easee extends utils.Adapter {
             } else {
                 //Value is in ioBroker, update it
                 const tmpValueId = data.mid + data_name;
-                this.log.info('New value over SignalR for: ' + tmpValueId + ', value: ' + data.value);
+                this.log.debug('New value over SignalR for: ' + tmpValueId + ', value: ' + data.value);
                 switch (data.dataType) {
                     case 2:
                         data.value = data.value == '1';
